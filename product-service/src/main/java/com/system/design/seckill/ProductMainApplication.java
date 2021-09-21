94933543af2de5016f961532d2c2c4389f971c68
tukangzheng001 <tukangzheng001@ke.com>
2021-09-21 17:40:37 +0800

    add annoatation

--- product-service/src/main/java/com/system/design/seckill/ProductMainApplication.java
+++ product-service/src/main/java/com/system/design/seckill/ProductMainApplication.java
@@ -1,8 +1,12 @@
 package com.system.design.seckill;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ProductMainApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(ProductMainApplication.class, args);
     }
 }
