6e0a1952598ca5c28af487f181ab9a3e925e3b40
xingdong <2473813003@qq.com>
2021-09-21 13:47:13 +0800

    update

--- seckill-design-service/src/main/resources/stock.lua
+++ seckill-design-service/src/main/resources/stock.lua
@@ -1,6 +1,6 @@
 if (redis.call('exists', KEYS[1]) == 1) then
     local stock = tonumber(redis.call('hget', KEYS[1], KEYS[2]));
-    if stock < 0 then
+    if stock <= 0 then
         return -1;
     end
     redis.call('HINCRBY', KEYS[1], KEYS[2], -1);
