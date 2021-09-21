dd0a77394194235c40290da3abec7234dd500561
xingdong <2473813003@qq.com>
2021-09-21 11:29:56 +0800

    update

--- seckill-design-service/src/main/java/com/system/design/seckill/config/BeanConfig.java
+++ seckill-design-service/src/main/java/com/system/design/seckill/config/BeanConfig.java
@@ -5,12 +5,14 @@ import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 /**
  * @author chengzhengzheng
  * @date 2021/9/19
  */
 @Configurable
+@Component
 public class BeanConfig {
 
     @Bean
