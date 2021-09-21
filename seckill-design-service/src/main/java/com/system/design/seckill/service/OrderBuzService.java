6774ecb55db9d36dabe5140537fecdc1fd5190fd
xingdong <2473813003@qq.com>
2021-09-21 22:44:19 +0800

    update

--- seckill-design-service/src/main/java/com/system/design/seckill/service/OrderBuzService.java
+++ seckill-design-service/src/main/java/com/system/design/seckill/service/OrderBuzService.java
@@ -9,11 +9,8 @@ import com.system.design.seckill.job.ScheduleJob;
 import com.system.design.seckill.utils.JsonUtils;
 import com.system.design.seckill.utils.KillEventTopiEnum;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.rocketmq.client.exception.MQBrokerException;
-import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.common.message.Message;
-import org.apache.rocketmq.remoting.exception.RemotingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
