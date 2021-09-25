6ea244589a9e2742f7c6a1bdc832a03a22d7871d
czb <k995211314@163.com>
2021-09-25 17:39:09 +0800

    mq

--- seckill-design-service/src/main/java/com/system/design/seckill/service/mq/DefaultConsumerMQ.java
+++ seckill-design-service/src/main/java/com/system/design/seckill/service/mq/DefaultConsumerMQ.java
@@ -1,6 +1,7 @@
 package com.system.design.seckill.service.mq;
 
 import com.system.design.seckill.config.mq.AbstractRocketConsumer;
+import com.system.design.seckill.utils.KillEventTopiEnum;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.springframework.stereotype.Component;
@@ -14,7 +15,7 @@ public class DefaultConsumerMQ extends AbstractRocketConsumer {
     @Override
     public void init() {
         // 设置主题,标签与消费者标题
-        super.necessary("topic_order", "*", "order标题");
+        super.necessary(KillEventTopiEnum.KILL_SUCCESS.getTopic(), "*", "order标题");
         //消费者具体执行逻辑
         registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
             msgs.forEach(msg -> System.out.printf("consumer message boyd %s %n", new String(msg.getBody())));
