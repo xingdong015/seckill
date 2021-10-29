dc1085b590efb5c8704b2a6a96f1b76048e09f50
xingdong <2473813003@qq.com>
2021-10-29 08:35:11 +0800

    commit

---
+++ seckill-design-dependency/src/main/java/com/system/design/seckill/SeckillDesignDependencyApplication.java
@@ -0,0 +1,13 @@
+package com.system.design.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillDesignDependencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillDesignDependencyApplication.class, args);
+    }
+
+}
