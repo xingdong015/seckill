4fa056c12deb93cd6a541d1432f16136bc6e9384
jiakai <jk@cnic.cn>
2021-11-08 18:59:51 +0800

    create seckill-product-service prpject

---
+++ seckill-dubbo-service/seckill-product-service/src/main/java/com/system/design/seckill/mapper/ProductMapper.java
@@ -0,0 +1,12 @@
+package com.system.design.seckill.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.system.design.seckill.common.entity.Product;
+
+/**
+ * @description:
+ * @author: 贾凯
+ * @create: 2021-11-08 18:34
+ */
+public interface ProductMapper extends BaseMapper<Product> {
+}
