121d97d5e75df3af7fee8677635a1773dd960ebd
xingdong <2473813003@qq.com>
2021-12-11 22:52:52 +0800

    debug

--- seckill-dubbo-service/seckill-order-service/rocketmq.md
+++ seckill-dubbo-service/seckill-order-service/rocketmq.md
@@ -1,12 +1,11 @@
 1. rocketmq事务消息原理: https://help.aliyun.com/document_detail/43348.html
 2. rocketmq 安装: https://rocketmq.apache.org/docs/quick-start/
 3. rocketmq基本原理： http://rocketmq.apache.org/docs/core-concept/
+4. rocketmq控制台： https://github.com/apache/rocketmq-dashboard
 
 > producer group概念：     Producers of the same role are grouped together. A different producer instance of the same producer group may be contacted by a broker to commit or roll back a transaction in case the original producer crashed after the transaction.
 
 同一个进程中的consumer Group必须是完全一样的订阅topic, 也就是说同一个consumer消费者实例、如果consumerGroup是相同的、那么他们的topic也必须是想通的。
 参考OrderListener.java 和 RocketMQMessageListener的配置
-
 Warning: consumer instances of a consumer group must have exactly the same topic subscription(s).
 
-rocketmq控制台： https://github.com/apache/rocketmq-dashboard
--- seckill-dubbo-service/seckill-product-service/src/main/java/com/system/design/seckill/product/mq/consumer/ShunXuRockerMqListener.java
+++ seckill-dubbo-service/seckill-product-service/src/main/java/com/system/design/seckill/product/mq/consumer/ShunXuRockerMqListener.java
@@ -1,26 +1,26 @@
-//package com.system.design.seckill.product.mq.consumer;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.apache.rocketmq.spring.annotation.ConsumeMode;
-//import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
-//import org.apache.rocketmq.spring.core.RocketMQListener;
-//import org.springframework.stereotype.Component;
-//
-///**
-// * @description:顺序消费：
-// * 转账等业务时需要保证消息顺序性，RocketMQ顺序性不是消费者保证，而是生产者保证。
-// * 生产者发送消息时需要指定路由Key，RocketMQ会自动分配到同一MessageQueue中。
-// * @author: 贾凯
-// * @create: 2021-11-22 17:34
-// */
-//@Slf4j
-//@Component
-//@RocketMQMessageListener(topic = "JK-Topic",consumerGroup = "product-group-consumer222",
-//                consumeMode = ConsumeMode.ORDERLY)
-//public class ShunXuRockerMqListener implements RocketMQListener<String> {
-//
-//    @Override
-//    public void onMessage(String s) {
-//        log.info("ShunXuRockerMqListener msg: {}",s);
-//    }
-//}
+package com.system.design.seckill.product.mq.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.spring.annotation.ConsumeMode;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @description:顺序消费：
+ * 转账等业务时需要保证消息顺序性，RocketMQ顺序性不是消费者保证，而是生产者保证。
+ * 生产者发送消息时需要指定路由Key，RocketMQ会自动分配到同一MessageQueue中。
+ * @author: 贾凯
+ * @create: 2021-11-22 17:34
+ */
+@Slf4j
+@Component
+@RocketMQMessageListener(topic = "JK-Topic",consumerGroup = "product-group-consumer222",
+                consumeMode = ConsumeMode.ORDERLY)
+public class ShunXuRockerMqListener implements RocketMQListener<String> {
+
+    @Override
+    public void onMessage(String s) {
+        log.info("ShunXuRockerMqListener msg: {}",s);
+    }
+}
