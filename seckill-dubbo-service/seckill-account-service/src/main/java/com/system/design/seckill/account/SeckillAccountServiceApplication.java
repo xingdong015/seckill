1f0d25bfc35b86eba6aaba13a7a7381137288478
chengzhengzheng.ch <chengzhengzheng@alibaba-inc.com>
2021-12-13 17:43:33 +0800

    update

---
+++ seckill-dubbo-service/seckill-account-service/src/main/java/com/system/design/seckill/account/SeckillAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.system.design.seckill.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillAccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillAccountServiceApplication.class, args);
+    }
+
+}
