d4f34417040f265c55e8ef5c04e447fe70feb9af
xingdong <2473813003@qq.com>
2021-12-13 22:54:41 +0800

    index

--- seckill-design-web/src/main/java/com/system/design/seckill/SeckillDesignWebApplication.java
+++ seckill-design-web/src/main/java/com/system/design/seckill/SeckillDesignWebApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.system.design.seckill.db.mapper")
 public class SeckillDesignWebApplication {
 
     public static void main(String[] args) {
--- seckill-design-web/src/main/java/com/system/design/seckill/web/IndexController.java
+++ seckill-design-web/src/main/java/com/system/design/seckill/web/IndexController.java
@@ -1,19 +1,15 @@
 package com.system.design.seckill.web;
 
 import com.system.design.seckill.common.api.IKillBuzService;
-import com.system.design.seckill.common.dto.ProductDto;
 import com.system.design.seckill.common.dto.SeckillDto;
-import com.system.design.seckill.common.po.Seckill;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author chengzhengzheng
  * @date 2021/10/26
  */
-@RestController
 @RequestMapping("/v1")
 public class IndexController {
     @DubboReference
@@ -23,5 +19,11 @@ public class IndexController {
     public void addKill(@RequestBody SeckillDto seckillDto) {
         killBuzService.addKill(seckillDto);
     }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
 
 }
