967b57882a74bc90fc615c950b8f5701b4145b89
chengzhengzheng.ch <chengzhengzheng@alibaba-inc.com>
2021-12-15 20:09:16 +0800

    fixBug

--- seckill-design-web/src/main/java/com/system/design/seckill/web/IndexController.java
+++ seckill-design-web/src/main/java/com/system/design/seckill/web/IndexController.java
@@ -11,13 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author chengzhengzheng
  * @date 2021/10/26
  */
-@RequestMapping("/v1")
 @Controller
 public class IndexController {
 
     @RequestMapping("/index")
     public String index() {
-        return "index.jsp";
+        return "index1";
     }
 
 
