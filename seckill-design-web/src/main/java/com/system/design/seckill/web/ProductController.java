35dc90e98899c7e588dfe41491249240f72a91ae
泽睿 <chengzhengzheng.ch@alibaba-inc.com>
2022-01-29 16:41:49 +0800

    gateway网关

--- seckill-design-web/src/main/java/com/system/design/seckill/web/ProductController.java
+++ seckill-design-web/src/main/java/com/system/design/seckill/web/ProductController.java
@@ -7,20 +7,20 @@ import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Random;
 
 /**
  * @author chengzhengzheng
  * @date 2021/10/26
  */
 @Controller
-public class IndexController {
+@RequestMapping("/product")
+public class ProductController {
 
     /**
      * 这里如果不添加这个 version=1.0.0 会导致注入失败问题、排查了好久、
@@ -29,25 +29,16 @@ public class IndexController {
     @DubboReference(version = "1.0.0")
     private IAccountService accountService;
 
-    //https://cxymm.net/article/weixin_45480785/118227209
-
-    @RequestMapping("/index")
-    public String index(Model model) {
-        //1. 下发所有活动中的 product
-        //2. 端上根据倒计时开始执行秒杀活动
-        //3.
-        List<Object> goods = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            Map<String, Object> good = new HashMap<>();
-            good.put("productName", "iphone" + i);
-            good.put("price", new Random().nextInt());
-            good.put("time", new Random().nextInt());
-            good.put("id", i);
-            goods.add(good);
-        }
+    @RequestMapping("/list")
+    public String list(Model model) {
+        final List<Object> goods = new ArrayList<>();
         model.addAttribute("goods", goods);
         return "product_list";
     }
 
+    @RequestMapping("/detail")
+    public String detail(@RequestParam("id") String id) {
+        return "";
+    }
 
 }
---
+++ seckill-design-web/src/main/java/com/system/design/seckill/web/SeckillController.java
@@ -0,0 +1,17 @@
+package com.system.design.seckill.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author chengzhengzheng
+ * @date 2022/1/29
+ */
+@Controller
+@RequestMapping("/seckill")
+public class SeckillController {
+
+    public void doSeckill(){
+
+    }
+}
