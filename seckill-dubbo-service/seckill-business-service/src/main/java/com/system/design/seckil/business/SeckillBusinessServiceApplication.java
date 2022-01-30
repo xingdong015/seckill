a55727cdebd25c9451931425540344f2a224877f
泽睿 <chengzhengzheng.ch@alibaba-inc.com>
2022-01-30 19:21:15 +0800

    添加本地缓存caffeine(一)

--- seckill-dubbo-service/seckill-business-service/src/main/java/com/system/design/seckil/business/SeckillBusinessServiceApplication.java
+++ seckill-dubbo-service/seckill-business-service/src/main/java/com/system/design/seckil/business/SeckillBusinessServiceApplication.java
@@ -3,10 +3,12 @@ package com.system.design.seckil.business;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCaching
 public class SeckillBusinessServiceApplication {
 
     public static void main(String[] args) {
